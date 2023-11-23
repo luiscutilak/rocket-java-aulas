d0f804a75c6cf712cc30f20ae9b344f81fb516cb
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-11-23 19:01:31 -0300

    initial commit

---
+++ EstruturasRepeticao.java
@@ -0,0 +1,20 @@
+class EstruturasRepeticao {
+
+    public static void main(String[] args) {
+
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 89879847324823L;
+        String dadDOTipoString = "Colocar o meu texto";
+        boolean dadoDoTipoBoolean = false;
+
+        if (dadoDoTipoInt == 10) {
+            System.out.println("entrou no if");
+        } else {
+            System.out.println("não entrou no if");
+        }
+
+    }
+
+}
